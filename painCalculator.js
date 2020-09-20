0a78dcf91ba13a0b8df3ee64fdc55c1ce2d15273
Katherine Tran <kathytran@Katherines-MacBook-Pro-2.local>
2020-09-20 13:03:20 -0700

    Pain Calculators

---
+++ painCalculator.js
@@ -0,0 +1,29 @@
+// function show_image(src, width, height) {
+//     screamingzenitsu = document.createElement("screamingzenitsu.jpeg");
+//     screamingzenitsu.src = screamingzenitsu.jpeg;
+//     screamingzenitsu.width = 300;
+//     screamingzenitsu.height = 158;
+//     document.body.appendChild(screamingzenitsu);
+// }
+
+function howMuchPainDoYouFeel(emotional, physical){
+    emotional = parseInt(emotional)
+    physical = parseInt(physical)
+    if( !document.getElementById("image1") ) {
+        var y = document.createElement("IMG");
+        y.id = "image1"
+        document.body.appendChild(y);
+    if (physical + emotional > 9000) {
+        document.getElementById("determine").innerHTML = "Its over 9000";
+        document.getElementById("image1").src = 'file:///Users/kathytran/Documents/I%20Want%20to%20Play%20With%20Code/Pain%20Calculator/cryingzenitsu.jpg'
+    }
+    if (physical > emotional && physical + emotional < 9000){
+            document.getElementById("determine").innerHTML = "Go to sleep.";
+            document.getElementById("image1").src = 'file:///Users/kathytran/Documents/I%20Want%20to%20Play%20With%20Code/Pain%20Calculator/screamingzenitsu.jpeg'
+        }
+    if (emotional>physical && physical + emotional < 9000){
+            document.getElementById("determine").innerHTML = "I want to sleep forever.";
+            document.getElementById("image1").src = 'file:///Users/kathytran/Documents/I%20Want%20to%20Play%20With%20Code/Pain%20Calculator/sufferingzenitsu.jpg'
+        }
+    }
+}
